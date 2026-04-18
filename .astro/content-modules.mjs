95ee4324ab81f92fc0ba66af638e9c5807cafa4e
dongheeHan <ark182818@gmail.com>
2026-04-18 17:39:52 +0900

    chore: remove node_modules from git, respect .gitignore

--- .astro/content-modules.mjs
+++
@@ -1,5 +0,0 @@
-
-export default new Map([
-["src/content/posts/spring-aop-proxy-mechanics.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fspring-aop-proxy-mechanics.mdx&astroContentModuleFlag=true")],
-["src/content/posts/attention-sqrt-d-scaling.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fattention-sqrt-d-scaling.mdx&astroContentModuleFlag=true")]]);
-		
